d9061dffcb2a323e1bf07adc5de35e9673f8421c
S71-GAZi <71398884+S71-GAZi@users.noreply.github.com>
2022-06-19 20:52:21 +0600

    RabbitProject

---
+++ Email.Api/Models/MailRequest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+
+namespace Email.Api.Models
+{
+    public class MailRequest
+    {
+        public string ToEmail { get; set; }
+        public string Cc { get; set; } = string.Empty;
+        public string Bcc { get; set; } = string.Empty;
+
+        public string Subject { get; set; } = string.Empty ;
+        public string Body { get; set; }
+        public List<IFormFile> Attachments { get; set; }
+    }
+}
